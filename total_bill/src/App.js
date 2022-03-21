0facac2308a2eb531f6147bb9618fd4e19c00817
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2022-03-21 23:10:01 +0530

    Add files via upload

---
+++ total_bill/src/App.js
@@ -0,0 +1,39 @@
+import React ,{useState} from "react";
+import "./style.css";
+
+export default function App() {
+  const [cost,setCost]=useState(0);
+  const [tip,setTip]=useState(0); 
+  const [result,setResult]=useState(0); 
+  const calculate=(e)=>{
+    e.preventDefault();
+setResult(eval(cost + tip)); 
+
+
+  }
+
+  return (
+    <div>
+      <label> Enter your bill </label> 
+      <br/>
+
+      <input type="number" className="bill" onChange={(e)=>setCost(Number(e.target.value))}/>
+
+      <br/>
+      <br/>
+      <label> Enter the tip</label> 
+      <br/> 
+
+      <input type="number" className="tip"  onChange={(e)=>setTip(Number(e.target.value))}/>
+      <br/> 
+      <button type="submit" onClick={calculate} >Calculate</button> 
+
+
+      <p>The final bill is {result}</p> 
+
+      <br/>
+
+
+    </div>
+  );
+}
